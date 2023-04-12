e440bbbe0b28b6ffe1acc1d6da9ab2852672aaa1
SongJae <songjae97@naver.com>
2023-04-12 11:56:16 +0000

    add molx

---
+++ molx/model/__init__.py
@@ -0,0 +1,12 @@
+from .deepergcn_dagnn_coords import Deepergcn_dagnn_coordinate
+from .schnet import SchNet
+from .schnet_2d import SchNet2D
+
+
+
+__all__ = [
+    "Deepergcn_dagnn_coordinate",
+    "make_mask",
+    "SchNet",
+    "SchNet2D"
+]
